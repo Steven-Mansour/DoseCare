7016c0577b17bf8a66dfee79977f44c29cb6f6c2
Steven-Mansour <stevenmansour0@gmail.com>
2025-01-25 23:16:05 +0200

    Add .env file

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 
 db = SQLAlchemy()
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://stvn:password@localhost/dosecaredb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     # Disable event system for performance
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
