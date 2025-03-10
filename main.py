39fa936b100004aa2f981e4d2c51545349cb1475
Steven-Mansour <99586951+Steven-Mansour@users.noreply.github.com>
2025-03-10 19:28:57 +0200

    Frontend fixes

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def index():
 
 @main.route('/hey')
 def hey():
-    # patient = Patient.query.filter_by(patientID=31).first()
-    caregiver = Caregiver.query.filter_by(caregiverID=13).first()
-    caregiver.get_lowest_pills_schedule()
-    return "hey"
-    # return patient.send_schedule()
+    patient = Patient.query.filter_by(patientID=2).first()
+    # caregiver = Caregiver.query.filter_by(caregiverID=13).first()
+    # caregiver.get_lowest_pills_schedule()
+    # return "hey"
+    return patient.send_schedule()
 
 
 @main.route('/home')
